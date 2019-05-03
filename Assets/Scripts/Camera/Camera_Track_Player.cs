fef35de26a877288c2a092b4e49b40808b230fd9
DavidR1997 <david.rowsellwork@gmail.com>
2019-05-03 15:56:50 +1000

    Initial commit

---
+++ Assets/Scripts/Camera/Camera_Track_Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Track_Player : MonoBehaviour
+{
+    public Transform target;
+    private Camera trackingCamera;
+    public float smoothing = 0.2f;
+    private Vector3 velocity = Vector3.zero;
+
+
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 targetPos = target.position;
+        targetPos.y = transform.position.y;
+        //smooths camera movement
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothing);
+    }
+}
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private CharacterController charC;
+    private Vector3 motion;
+    private Camera cam;
+    void Start()
+    {
+        charC = GetComponent<CharacterController>();
+        cam = FindObjectOfType<Camera>();
+    }
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+        Move(inputH, inputZ);
+        charC.Move(motion * Time.deltaTime);
+
+        Ray cameraRay = cam.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength = 100f;
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 hitPoint = cameraRay.GetPoint(rayLength);
+            transform.LookAt(new Vector3(hitPoint.x, transform.position.y, hitPoint.z));
+        }
+
+
+    }
+    void Move(float inputH, float inputZ)
+    {
+        Vector3 direction = new Vector3(inputH, 0f, inputZ);
+
+        motion.x = direction.x * speed;
+        motion.z = direction.z * speed;
+
+    }
+}
