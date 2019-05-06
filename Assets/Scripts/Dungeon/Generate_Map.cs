5411788a61be5f482401366067337f4442e0832e
DavidR1997 <david.rowsellwork@gmail.com>
2019-05-06 22:49:41 +1000

    Room Prefabs

---
+++ Assets/Scripts/Dungeon/Generate_Map.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate_Map : MonoBehaviour
+{
+    public List<GameObject> corridorsToSpawn;
+    public List<GameObject> roomsToSpawn;
+    public GameObject spawnRoom;
+    public GameObject exitRoom;
+    Vector3 dungeonSpawnLocation = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
