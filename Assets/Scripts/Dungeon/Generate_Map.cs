74f2b96f4cab14fdbd6f98c65978f66d3f667f21
DavidR1997 <david.rowsellwork@gmail.com>
2019-05-08 22:58:27 +1000

    f

--- Assets/Scripts/Dungeon/Generate_Map.cs
+++ Assets/Scripts/Dungeon/Generate_Map.cs
@@ -9,15 +9,28 @@ public class Generate_Map : MonoBehaviour
     public GameObject spawnRoom;
     public GameObject exitRoom;
     Vector3 dungeonSpawnLocation = Vector3.zero;
+
+    private int mapSeed = 123456789;
+    private bool isMapGenerated;
+    int maxRoomsToGen = 25;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Random.InitState(mapSeed);
+        CreateRooms();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void CreateRooms()
+    {
+        //spawn the spawn room at zero vector
+        Instantiate(spawnRoom, dungeonSpawnLocation, Quaternion.identity);
+        this.roomsToSpawn = new List<GameObject>();
+        this.corridorsToSpawn = new List<GameObject>();
     }
 }
