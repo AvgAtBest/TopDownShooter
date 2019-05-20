b1fecc41efda32f50954bbf0370f6b98309507ea
DavidR1997 <david.rowsellwork@gmail.com>
2019-05-20 18:24:44 +1000

    stuff

---
+++ Assets/Scripts/Player/Player_Interaction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Interaction : MonoBehaviour
+{
+    private Camera cam;
+    public Transform muzzleSpawn;
+    public GameObject bulletTest;
+    void Start()
+    {
+        muzzleSpawn = GameObject.Find("Muzzle").GetComponentInChildren<Transform>();
+        cam = FindObjectOfType<Camera>();
+    }
+
+    void Update()
+    {
+        //Look at wherre mouse is
+        Ray cameraRay = cam.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength = 100f;
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 hitPoint = cameraRay.GetPoint(rayLength);
+            transform.LookAt(new Vector3(hitPoint.x, transform.position.y, hitPoint.z));
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Debug.Log("Shooting");
+        
+        GameObject clone = Instantiate(bulletTest, muzzleSpawn.position, muzzleSpawn.rotation);
+        clone.AddComponent<Rigidbody>();
+
+    }
+}
---
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float diveSpeed = 20f;
+    private CharacterController charC;
+    private Vector3 motion;
+    private Camera cam;
+    float counter;
+    bool endReached;
+    float gravity = 15f;
+    void Start()
+    {
+        charC = GetComponent<CharacterController>();
+        cam = FindObjectOfType<Camera>();
+        counter = 0;
+    }
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+        
+        
+        Move(inputH, gravity, inputZ);
+        charC.Move(motion * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+
+        }
+
+
+    }
+    void Move(float inputH,float gravity, float inputZ)
+    {
+        Vector3 direction = new Vector3(inputH, gravity, inputZ);
+        motion.x = direction.x * speed;
+        //shit gravity
+        //motion.y -= direction.y - gravity * Time.deltaTime;
+        motion.z = direction.z * speed;
+
+
+    }
+    void Dive()
+    {
+
+        motion = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        motion = transform.TransformDirection(motion);
+        motion *= diveSpeed;
+        motion.z = diveSpeed;
+        motion.y -= gravity * Time.deltaTime;
+        #region Terrible
+        //Vector3 startPos = transform.position;
+        //Vector3 pos = new Vector3(startPos.x, startPos.y + Mathf.Sin(Mathf.PI * 2 * counter / 360), startPos.z + Mathf.Sin(Mathf.PI * 2 * counter / 360));
+        //transform.position = Vector3.Lerp(transform.position, pos, 1f);
+        //counter += speed;
+        //if(counter >= 180)
+        //{
+        //    endReached = true;
+        //}
+        #endregion
+
+    }
+}
