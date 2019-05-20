e69cf58b78bf31164c9994fafce36dd39f03f335
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2019-05-21 00:12:24 +1000

    DiveInProgress
    Enemy completed, Dive is still being worked on

---
+++ Assets/Scripts/Player/PlayerDive.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerDive : MonoBehaviour
+{ 
+   
+
+
+    public Vector3 dive;
+    public float force = 2.0f;
+    public bool isGrounded;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        dive = new Vector3(0.0f, 2.0f, 2.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(dive * force, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+
+}
