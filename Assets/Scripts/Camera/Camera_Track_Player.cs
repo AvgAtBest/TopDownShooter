394c396f31828b5338d772df7c5890c4c3b9ed68
David <david.rowsellwork@gmail.com>
2019-05-29 16:24:31 +1000

    g

--- Assets/Scripts/Camera/Camera_Track_Player.cs
+++ Assets/Scripts/Camera/Camera_Track_Player.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class Camera_Track_Player : MonoBehaviour
 {
+    [Header("Follow")]
     public Transform target;
-    private Camera trackingCamera;
+    public Camera trackingCamera;
+    public Vector3 offset;
+    [Header("Smoothing")]
     public float smoothing = 0.2f;
-    private Vector3 velocity = Vector3.zero;
-
-
+    public Vector3 velocity = Vector3.zero;
     void Start()
     {
         target = GameObject.Find("Player").GetComponent<Transform>();
+        offset = transform.position - target.position;
+        
+        trackingCamera = this.GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 targetPos = target.position;
-        targetPos.y = transform.position.y;
+        transform.position = target.position + offset;
+        transform.LookAt(target.position);
+        //Vector3 targetPos = target.position;
+        //targetPos.y = transform.position.y;
         //smooths camera movement
-        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothing);
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothing);
+    }
+    private void FixedUpdate()
+    {
+
     }
 }
