ff0b4209c45a1d90f3e5bbed582648534e8941e9
DavidR1997 <david.rowsellwork@gmail.com>
2019-05-30 16:47:54 +1000

    stuff

--- Assets/Scripts/Camera/Camera_Track_Player.cs
+++ Assets/Scripts/Camera/Camera_Track_Player.cs
@@ -4,34 +4,55 @@ using UnityEngine;
 
 public class Camera_Track_Player : MonoBehaviour
 {
-    [Header("Follow")]
-    public Transform target;
-    public Camera trackingCamera;
-    public Vector3 offset;
-    [Header("Smoothing")]
-    public float smoothing = 0.2f;
-    public Vector3 velocity = Vector3.zero;
-    public float turnSpeed = 2f;
-    void Start()
-    {
-        target = GameObject.Find("Player").GetComponent<Transform>();
-        offset = transform.position - target.position;
-        
-        trackingCamera = this.GetComponent<Camera>();
-    }
+	[Header("Follow")]
+	public Transform target;
+	public Camera trackingCamera;
+	public Vector3 offset;
+	[Header("Smoothing")]
+	public float smoothing = 0.2f;
+	public Vector3 velocity = Vector3.zero;
+	public float turnSpeed = 2f;
+	void Start()
+	{
+		target = GameObject.Find("Player").GetComponent<Transform>();
+		//offset = transform.position - target.position;
+		
+		trackingCamera = this.GetComponent<Camera>();
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	//void FixedUpdate()
+	//{
+	//	//trackingCamera.transform.localRotation = Quaternion.Euler(new Vector3(0, target.localRotation.y, 0));
+	//	Ray ray = trackingCamera.ScreenPointToRay(Input.mousePosition);
+	//	RaycastHit hit;
+	//	if(Physics.Raycast(ray, out hit))
+	//	{
+	//		Vector3 average = (hit.point + target.position) / 2;
 
-        Vector3 targetPos = target.position;
-        targetPos.y = transform.position.y;
-        //smooths camera movement
-        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothing);
-        #region rotation
-        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-        transform.position = target.position + offset;
-        transform.LookAt(target);
-        #endregion
-    }
+	//		average.y = transform.position.y;
+
+	//		//smooths camera movement
+	//		transform.position = Vector3.SmoothDamp(transform.position, average, ref velocity, smoothing);
+	//	}
+
+	//}
+	private void FixedUpdate()
+	{
+		Vector3 averagePos = target.position;
+		averagePos.y = transform.position.y;
+		transform.position = Vector3.SmoothDamp(transform.position, averagePos, ref velocity, smoothing);
+	}
+	private void Update()
+	{
+		
+	}
+	//private void LateUpdate()
+	//{
+	//	//#region rotation
+	//	transform.position = target.position + offset;
+	//	transform.LookAt(target.position);
+	//	//#endregion
+	//}
 }
--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -4,38 +4,41 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    public float speed = 10f;
+	public float speed = 10f;
 
-    private Rigidbody rigid;
-    private Vector3 motion;
-    private Camera cam;
-    public bool isTopDown;
-    void Start()
-    {
-        rigid = GetComponent<Rigidbody>();
-        cam = FindObjectOfType<Camera>();
-        rigid.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-    }
-    void Update()
-    {
-        float inputH = Input.GetAxis("Horizontal") * speed;
-        float inputZ = Input.GetAxis("Vertical") * speed;
+	private Rigidbody rigid;
+	private Vector3 motion;
+	private Camera cam;
+	public bool isTopDown;
+	void Start()
+	{
+		rigid = GetComponent<Rigidbody>();
+		cam = FindObjectOfType<Camera>();
+		rigid.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+	}
+	void Update()
+	{
+		float inputH = Input.GetAxis("Horizontal") * speed;
+		float inputZ = Input.GetAxis("Vertical") * speed;
 
-        Vector3 moveDir = new Vector3(inputH, 0f, inputZ);
-        Vector3 camEuler = Camera.main.transform.eulerAngles;
-        moveDir = Quaternion.AngleAxis(camEuler.y, Vector3.up) * moveDir;
-        Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
+		Vector3 moveDir = new Vector3(inputH, 0f, inputZ);
+		//moveDir = transform.TransformDirection(moveDir);
 
-        rigid.velocity = force;
-        Quaternion playerRot = Quaternion.AngleAxis(camEuler.y, Vector3.up);
-        transform.rotation = playerRot;
-    }
-    //void Move(float inputH, float inputZ)
-    //{
-    //    Vector3 direction = new Vector3(inputH, 0f, inputZ);
+		Vector3 camEuler = cam.transform.localEulerAngles;
+		moveDir = Quaternion.AngleAxis(camEuler.y, Vector3.up) * moveDir;
 
-    //    motion.x = direction.x * speed;
-    //    motion.z = direction.z * speed;
+		Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
 
-    //}
+		rigid.velocity = force;
+		Quaternion playerRot = Quaternion.AngleAxis(camEuler.y, Vector3.up);
+		transform.rotation = playerRot;
+	}
+	//void Move(float inputH, float inputZ)
+	//{
+	//    Vector3 direction = new Vector3(inputH, 0f, inputZ);
+
+	//    motion.x = direction.x * speed;
+	//    motion.z = direction.z * speed;
+
+	//}
 }
