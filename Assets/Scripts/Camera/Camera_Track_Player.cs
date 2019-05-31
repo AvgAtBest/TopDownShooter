d6277f02087652da86c4ac404c6db024e014f2d9
AveAmicus2250 <42423943+AveAmicus2250@users.noreply.github.com>
2019-05-31 17:21:38 +1000

    ...
    ...

--- Assets/Scripts/Camera/Camera_Track_Player.cs
+++ Assets/Scripts/Camera/Camera_Track_Player.cs
@@ -21,38 +21,35 @@ public class Camera_Track_Player : MonoBehaviour
 		
 	}
 
-	// Update is called once per frame
-	//void FixedUpdate()
-	//{
-	//	//trackingCamera.transform.localRotation = Quaternion.Euler(new Vector3(0, target.localRotation.y, 0));
-	//	Ray ray = trackingCamera.ScreenPointToRay(Input.mousePosition);
-	//	RaycastHit hit;
-	//	if(Physics.Raycast(ray, out hit))
-	//	{
-	//		Vector3 average = (hit.point + target.position) / 2;
+    // Update is called once per frame
+    //void FixedUpdate()
+    //{
+    //    //trackingCamera.transform.localRotation = Quaternion.Euler(new Vector3(0, target.localRotation.y, 0));
+    //    Ray ray = trackingCamera.ScreenPointToRay(Input.mousePosition);
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(ray, out hit))
+    //    {
+    //        Vector3 average = (hit.point + target.position) / 2;
 
-	//		average.y = transform.position.y;
+    //        average.y = transform.position.y;
 
-	//		//smooths camera movement
-	//		transform.position = Vector3.SmoothDamp(transform.position, average, ref velocity, smoothing);
-	//	}
+    //        //smooths camera movement
+    //        transform.position = Vector3.SmoothDamp(transform.position, average, ref velocity, smoothing);
+    //    }
 
-	//}
-	private void FixedUpdate()
-	{
-		Vector3 averagePos = target.position;
-		averagePos.y = transform.position.y;
-		transform.position = Vector3.SmoothDamp(transform.position, averagePos, ref velocity, smoothing);
-	}
-	private void Update()
-	{
-		
-	}
-	//private void LateUpdate()
-	//{
-	//	//#region rotation
-	//	transform.position = target.position + offset;
-	//	transform.LookAt(target.position);
-	//	//#endregion
-	//}
+    //}
+    private void FixedUpdate()
+    {
+        Vector3 averagePos = target.position;
+        averagePos.y = transform.position.y;
+        transform.position = Vector3.SmoothDamp(transform.position, averagePos, ref velocity, smoothing);
+    }
+
+    //private void LateUpdate()
+    //{
+    //    //#region rotation
+    //    transform.position = target.position + offset;
+    //    transform.LookAt(target.position);
+    //    //#endregion
+    //}
 }
--- Assets/Scripts/Player/Player_Interaction.cs
+++ Assets/Scripts/Player/Player_Interaction.cs
@@ -33,8 +33,8 @@ public class Player_Interaction : MonoBehaviour
     {
         Debug.Log("Shooting");
         
-        GameObject clone = Instantiate(bulletTest, muzzleSpawn.position, muzzleSpawn.rotation);
-        clone.AddComponent<Rigidbody>();
+        //GameObject clone = Instantiate(bulletTest, muzzleSpawn.position, muzzleSpawn.rotation);
+        //clone.AddComponent<Rigidbody>();
 
     }
 }
