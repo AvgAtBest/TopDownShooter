f0e4051f6649fcc0ec5f3cd34784b2e5675c4e35
David <david.rowsellwork@gmail.com>
2019-06-05 12:11:01 +1000

    cam start orientation

--- Assets/Scripts/Camera/Camera_Track_Player.cs
+++ Assets/Scripts/Camera/Camera_Track_Player.cs
@@ -4,56 +4,69 @@ using UnityEngine;
 
 public class Camera_Track_Player : MonoBehaviour
 {
-	[Header("Follow")]
-	public Transform target;
-	public Camera trackingCamera;
-	public Vector3 offset;
-	[Header("Smoothing")]
-	public float smoothing = 0.2f;
-	public Vector3 velocity = Vector3.zero;
-	public float turnSpeed = 2f;
-	void Start()
-	{
-		target = GameObject.Find("Player").GetComponent<Transform>();
-		//offset = transform.position - target.position;
-		
-		trackingCamera = this.GetComponent<Camera>();
-		
+    [Header("Follow")]
+    public Transform target;
+    public Camera trackingCamera;
+    public Vector3 offset;
+    [Header("Smoothing")]
+    public float smoothing = 0.2f;
+    public Vector3 velocity = Vector3.zero;
+    public float turnSpeed = 2f;
+    public Vector3 startPos;
+    public float xRot = 90f;
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>();
+        //offset = transform.position - target.position;
 
-	}
+        trackingCamera = this.GetComponent<Camera>();
+        TopDownCamView();
+    }
 
-	// Update is called once per frame
-	//void FixedUpdate()
-	//{
-	//	//trackingCamera.transform.localRotation = Quaternion.Euler(new Vector3(0, target.localRotation.y, 0));
-	//	Ray ray = trackingCamera.ScreenPointToRay(Input.mousePosition);
-	//	RaycastHit hit;
-	//	if(Physics.Raycast(ray, out hit))
-	//	{
-	//		Vector3 average = (hit.point + target.position) / 2;
+    // Update is called once per frame
+    //void FixedUpdate()
+    //{
+    //	//trackingCamera.transform.localRotation = Quaternion.Euler(new Vector3(0, target.localRotation.y, 0));
+    //	Ray ray = trackingCamera.ScreenPointToRay(Input.mousePosition);
+    //	RaycastHit hit;
+    //	if(Physics.Raycast(ray, out hit))
+    //	{
+    //		Vector3 average = (hit.point + target.position) / 2;
 
-	//		average.y = transform.position.y;
+    //		average.y = transform.position.y;
 
-	//		//smooths camera movement
-	//		transform.position = Vector3.SmoothDamp(transform.position, average, ref velocity, smoothing);
-	//	}
+    //		//smooths camera movement
+    //		transform.position = Vector3.SmoothDamp(transform.position, average, ref velocity, smoothing);
+    //	}
 
-	//}
-	private void FixedUpdate()
-	{
-		Vector3 averagePos = target.position;
-		averagePos.y = transform.position.y;
-		transform.position = Vector3.SmoothDamp(transform.position, averagePos, ref velocity, smoothing);
-	}
-	private void Update()
-	{
-		
-	}
-	//private void LateUpdate()
-	//{
-	//	//#region rotation
-	//	transform.position = target.position + offset;
-	//	transform.LookAt(target.position);
-	//	//#endregion
-	//}
+    //}
+    private void FixedUpdate()
+    {
+        Vector3 averagePos = target.localPosition;
+        averagePos.y = transform.position.y;
+        transform.position = Vector3.SmoothDamp(transform.position, averagePos, ref velocity, smoothing);
+    }
+    private void Update()
+    {
+        //Vector2 mouseLocation = Input.mousePosition;
+        //Vector3 screenPos = Camera.main.ScreenToWorldPoint(new Vector3(mouseLocation.x, mouseLocation.y, transform.position.z - Camera.main.transform.position.z));
+        
+    }
+    private void TopDownCamView()
+    {
+        startPos = new Vector3(0, 15, 0);
+        trackingCamera.transform.position = startPos;
+        trackingCamera.transform.eulerAngles = new Vector3(xRot, 0, 0);
+    }
+    private void SideView()
+    {
+
+    }
+    //private void LateUpdate()
+    //{
+    //	//#region rotation
+    //	transform.position = target.position + offset;
+    //	transform.LookAt(target.position);
+    //	//#endregion
+    //}
 }
--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -6,6 +6,8 @@ public class Player_Movement : MonoBehaviour
 {
 	public float speed = 10f;
 
+    public float offset;
+
 	private Rigidbody rigid;
 	private Vector3 motion;
 	private Camera cam;
@@ -30,8 +32,17 @@ public class Player_Movement : MonoBehaviour
 		Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
 
 		rigid.velocity = force;
-		Quaternion playerRot = Quaternion.AngleAxis(camEuler.y, Vector3.up);
-		transform.rotation = playerRot;
+
+        Vector3 camPos = cam.WorldToScreenPoint(Input.mousePosition) - transform.position;
+        camPos.Normalize();
+        float rotY = Mathf.Atan2(camPos.x, camPos.z) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, rotY + offset, 0f);
+        //transform.LookAt(new Vector3(camPos.x, transform.position.y, camPos.z));
+        
+		//Quaternion playerRot = Quaternion.AngleAxis(camEuler.y, Vector3.up);
+
+		//transform.rotation = playerRot;
+
 	}
 	//void Move(float inputH, float inputZ)
 	//{
