0d835f9a6240bf15b7e0c359a8f2b71b39e8af2b
AveAmicus2250 <42423943+AveAmicus2250@users.noreply.github.com>
2019-06-06 17:45:53 +1000

    Additions
    Shoot and Loot

---
+++ Assets/DropItems.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItems : MonoBehaviour
+{
+    public List<GameObject> drops = new List<GameObject>();
+
+    private void Start()
+    {
+        DropRandomItem();
+    }
+
+    public void DropRandomItem()
+    {
+        print("Dropping a random item");
+        Instantiate(drops[Random.Range(0, drops.Count)], transform.position, Quaternion.identity);
+    }
+}
