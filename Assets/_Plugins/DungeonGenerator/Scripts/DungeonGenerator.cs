fea7bd0888a310330d7d816eae4e3408b63dc3ea
DavidR1997 <david.rowsellwork@gmail.com>
2019-06-14 15:36:25 +1000

    finalroom

--- Assets/_Plugins/DungeonGenerator/Scripts/DungeonGenerator.cs
+++ Assets/_Plugins/DungeonGenerator/Scripts/DungeonGenerator.cs
@@ -46,6 +46,11 @@ public class DungeonGenerator : MonoBehaviour
 	public int EndRoomsSpawned;
 	public int[] elementsToTry = { 1, 2, 3, };
 	public GameObject shop;
+	public GameObject key;
+	public bool keySpawned;
+	public int keysToSpawn = 1;
+	public List<Transform> keySpawnPoints = new List<Transform>();
+	
 	#region Start
 	void Start()
 	{
@@ -72,7 +77,7 @@ public class DungeonGenerator : MonoBehaviour
 		//Debug.Log("Using set: " + data.sets[dungeonSet].name);
 
 		StartGeneration();
-
+		
 
 	}
 	#endregion
@@ -104,7 +109,7 @@ public class DungeonGenerator : MonoBehaviour
 		while (openSet.Count > 0)
 		{
 			GenerateNextRoom();
-			return;
+			//return;
 		}
 
 		//process doors
@@ -275,18 +280,17 @@ public class DungeonGenerator : MonoBehaviour
 				{
 					Debug.Log("Oh well!");
 					//CAN GET MULTIPLE FINAL ROOMS TO SNAP HERE
-					if (hasFinalRoomSpawned == false)
-					{
 
-						// INDEX '0' to 100% room
-						roomToTry = finalRoomList[0].gameObject;
-
-						hasFinalRoomSpawned = true;
-
-						
-
-					}
 				}
+			}
+			if (hasFinalRoomSpawned == false)
+			{
+
+				// INDEX '0' to 100% room
+				roomToTry = finalRoomList[0].gameObject;
+
+				hasFinalRoomSpawned = true;
+
 			}
 
 			possibleRooms.RemoveAt(r);
@@ -514,323 +518,7 @@ public class DungeonGenerator : MonoBehaviour
 		}
 	}
 	#endregion
-	#region Final Room Spawn
-	void FinalSpawn(GameObject newRoom, Room lastRoom, GeneratorDoor door)
-	{
-		#region david Editions
 
-		//if (openSet.Count > 0) lastRoom = openSet[0];
-
-		//create a mutable list of all possible rooms
-
-
-		//List<Room> endElevatorRoom = new List<Room>();
-		//for (int i = 0; i < data.sets[dungeonSet].finalRooms.Count; i++)
-		//{
-		//    endElevatorRoom.Add(data.sets[dungeonSet].finalRooms[i]);
-		//}
-		List<Room> finalRoomList = new List<Room>();
-		for (int f = 0; f < data.sets[dungeonSet].finalRooms.Count; f++)
-		{
-			finalRoomList.Add(data.sets[dungeonSet].finalRooms[f]);
-		}
-
-		bool roomIsGood = false;
-
-		//Debug.Log("count: " + data.sets[dungeonSet].roomTemplates.Count);
-
-		do
-		{
-			for (int i = 0; i < doorVoxelsTest.Count; i++)
-			{
-				GameObject.DestroyImmediate(doorVoxelsTest[i]);
-			}
-			doorVoxelsTest.Clear();
-
-
-			//If we picked a room with with one door, try again UNLESS we've have no other rooms to try
-			int doors = 0;
-			//bool tryAgain = false;
-			GameObject roomToTry;
-			// endRoomToTry;
-			int r = random.range(0, finalRoomList.Count - 1);
-			//int p = random.range(0, finalRoomList.Count - 1); //// - ALIGNMENT TEST
-			////Debug.Log("r: " + r);
-			////Debug.Log(possibleRooms.Count);
-			roomToTry = finalRoomList[r].gameObject;
-			//roomToTry = finalRoomList[p].gameObject; ////- ALIGNMENT TEST
-			//endRoomToTry = endRoom;
-			doors = roomToTry.GetComponent<Room>().doors.Count;
-
-			//if (doors == 1 && finalRoomList.Count == 1) //&& finalRoomList.Count == 1)
-			//{
-			//	//Debug.Log("we're adding a room with one door when we have other's we could try first..");
-			//	float chance = 1f - Mathf.Sqrt(((float)roomsCount / (float)targetRooms)); //the closer we are to target the less of a chance of changing rooms
-			//	float randomValue = random.value();
-			//	//Debug.Log("Chance: " + chance + " | Random value: " + randomValue);
-			//	if (randomValue < chance)
-			//	{
-			//		r = random.range(0, finalRoomList.Count - 1);
-			//		roomToTry = finalRoomList[r].gameObject;
-			//		//Debug.Log("trying a new room");
-			//		//Debug.Log("New room has doors: " + roomToTry.GetComponent<Room>().doors.Count);
-
-			//		doors = roomToTry.GetComponent<Room>().doors.Count;
-			//		if (doors == 1 && finalRoomList.Count > 1) // && finalRoomList.Count == 1)
-			//		{
-			//			float chance2 = 1f - Mathf.Sqrt(((float)roomsCount / (float)targetRooms)); //the closer we are to target the less of a chance of changing rooms
-			//			float randomValue2 = random.value();
-			//			if (randomValue2 < chance2)
-			//			{
-			//				r = random.range(0, finalRoomList.Count - 1);
-			//				roomToTry = finalRoomList[r].gameObject;
-			//			}
-			//			else
-			//			{
-			//				Debug.Log("Oh well again..");
-			//				//int finalRoom = random.range(0, data.sets[dungeonSet].finalRooms.Count - 1);
-			//				//endRoom = (GameObject)Instantiate(data.sets[dungeonSet].finalRooms[finalRoom].gameObject);
-
-			//				//endRoom = possibleRooms[r].gameObject;
-			//			}
-
-			//		}
-			//	}
-			//	else
-			//	{
-			//		Debug.Log("Oh well!");
-			//	}
-			//}
-			//possibleRooms.RemoveAt(r);
-
-			newRoom = (GameObject)Instantiate(roomToTry);
-
-			Debug.Log("Room to try has spawned" + roomToTry.name + transform.position);
-			newRoom.transform.parent = this.gameObject.transform;
-			door = ConnectRooms(lastRoom, newRoom.GetComponent<Room>());
-
-			#region Global Voxel Check
-			//room is now generated and in position... we need to test overlap now!
-			Volume v = newRoom.GetComponent<Volume>();
-			Room ro = newRoom.GetComponent<Room>();
-			bool overlap = false;
-			for (int i = 0; i < v.voxels.Count; i++)
-			{
-				if (globalVoxels.ContainsKey(RoundVec3ToInt(v.voxels[i].gameObject.transform.position)))
-				{
-					//overlap found! bad!
-					//Debug.Log("THERE IS AN OVERLAP!!");
-					overlap = true;
-					continue;
-				}
-
-				for (int j = 0; j < openSet.Count; j++)
-				{
-					for (int k = 0; k < openSet[j].doors.Count; k++)
-					{
-						//check if door is in the globalVoxelList
-						if (!openSet[j].doors[k].open) continue;
-						//we also want to ignore the Door we're connecting with
-						if (openSet[j].doors[k] == door) continue;
-						float rot = NormalizeAngle(Mathf.RoundToInt(openSet[j].doors[k].transform.rotation.eulerAngles.y));
-						Vector3 direction = new Vector3();
-						if (rot == 0)
-						{
-							////Debug.Log("Door: " + i + " is facing: +X");
-							direction = new Vector3(1f, 0f, 0f);
-						}
-						else if (rot == 180)
-						{
-							////Debug.Log("Door: " + i + " is facing: -X");
-							direction = new Vector3(-1f, 0f, 0f);
-						}
-						else if (rot == 90)
-						{
-							////Debug.Log("Door: " + i + " is facing: -Z");
-							direction = new Vector3(0f, 0f, -1f);
-						}
-						else if (rot == 270)
-						{
-							////Debug.Log("Door: " + i + " is facing: +Z");
-							direction = new Vector3(0f, 0f, 1f);
-						}
-						GameObject g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-						g.transform.position = openSet[j].doors[k].voxelOwner.transform.position + (direction * v.voxelScale);
-						g.GetComponent<Renderer>().material.color = Color.red;
-						doorVoxelsTest.Add(g);
-						if (RoundVec3ToInt(v.voxels[i].gameObject.transform.position) == RoundVec3ToInt(openSet[j].doors[k].voxelOwner.transform.position + (direction * v.voxelScale)))
-						{
-							overlap = true;
-							Debug.Log("Room is overlapping a door voxel neighbour!!!");
-						}
-						else
-						{
-							Debug.Log("Room is NOT overlapping with a door voxel neighbour!");
-						}
-					}
-				}
-			}
-			#endregion
-			#region Overlap
-			bool hasSpace = true;
-			if (!overlap)
-			{
-				//Debug.Log("NO overlap with the room...checking doors");
-				//check all the doors, and make sure there's at leas a 1x1x1 voxel of air out of it
-				//this will enure we have room for a treasure room at least, and no doors will lead right into a wall!
-				for (int i = 0; i < ro.doors.Count; i++)
-				{
-					//we need to find the direction the door is pointing in world space..
-					//Debug.Log(i + " : " + ro.doors[i].open);
-					if (!ro.doors[i].open) continue; //check all OPEN doors BUT the one we're connecting with..
-					if (ro.doors[i] == newRoom.GetComponent<Room>().GetFirstOpenDoor()) continue;
-					//Debug.Log("Actually checking door: " + i);
-					float rot = NormalizeAngle(Mathf.RoundToInt(ro.doors[i].transform.rotation.eulerAngles.y));
-					Vector3 direction = new Vector3();
-					if (rot == 0)
-					{
-						////Debug.Log("Door: " + i + " is facing: +X");
-						direction = new Vector3(1f, 0f, 0f);
-					}
-					else if (rot == 180)
-					{
-						////Debug.Log("Door: " + i + " is facing: -X");
-						direction = new Vector3(-1f, 0f, 0f);
-					}
-					else if (rot == 90)
-					{
-						////Debug.Log("Door: " + i + " is facing: -Z");
-						direction = new Vector3(0f, 0f, -1f);
-					}
-					else if (rot == 270)
-					{
-						////Debug.Log("Door: " + i + " is facing: +Z");
-						direction = new Vector3(0f, 0f, 1f);
-					}
-					//Debug.Log("Drawing spheres");
-					GameObject g = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-					g.transform.position = ro.doors[i].voxelOwner.transform.position + (direction * v.voxelScale);
-					doorVoxelsTest.Add(g);
-
-					if (globalVoxels.ContainsKey(RoundVec3ToInt(ro.doors[i].voxelOwner.transform.position + (direction * v.voxelScale))))
-					{
-						//we have a collision on the door neighbours
-						//Debug.Log("WE HAVE A COLLISION WITH THE DOOR NEIGHBOURS");
-						hasSpace = false;
-						break;
-					}
-					else
-					{
-						//we good!
-						//Debug.Log("We don't have a collision witht he door neighbours");
-						//check doors against all other doors so that no door voxels overlap with other door  voxels
-						for (int j = 0; j < openSet.Count; j++)
-						{
-							for (int k = 0; k < openSet[j].doors.Count; k++)
-							{
-								if (!openSet[j].doors[k].open) continue;
-								float rot2 = NormalizeAngle(Mathf.RoundToInt(openSet[j].doors[k].transform.rotation.eulerAngles.y));
-								Vector3 direction2 = new Vector3();
-								if (rot2 == 0)
-								{
-									////Debug.Log("Door: " + i + " is facing: +X");
-									direction2 = new Vector3(1f, 0f, 0f);
-								}
-								else if (rot2 == 180)
-								{
-									////Debug.Log("Door: " + i + " is facing: -X");
-									direction2 = new Vector3(-1f, 0f, 0f);
-								}
-								else if (rot2 == 90)
-								{
-									////Debug.Log("Door: " + i + " is facing: -Z");
-									direction2 = new Vector3(0f, 0f, -1f);
-								}
-								else if (rot2 == 270)
-								{
-									////Debug.Log("Door: " + i + " is facing: +Z");
-									direction2 = new Vector3(0f, 0f, 1f);
-								}
-
-								if (RoundVec3ToInt(ro.doors[i].voxelOwner.transform.position + (direction * v.voxelScale)) == RoundVec3ToInt(openSet[j].doors[k].voxelOwner.transform.position + (direction2 * v.voxelScale)))
-								{
-									hasSpace = false;
-									//Debug.Log("TWo door voxels overlapping!");
-									break;
-								}
-							}
-							if (!hasSpace) break;
-						}
-					}
-				}
-			}
-
-			if (!overlap && hasSpace)
-			{
-				//Debug.Log("all next rooms will fit!");
-				roomIsGood = true;
-			}
-			else
-			{
-				GameObject.DestroyImmediate(newRoom);
-				//Debug.Log("Try a different room!!!!--------");
-				//destroy the room we just tried to place
-			}
-			#endregion
-		} while (finalRoomList.Count > 0 && !roomIsGood);
-		if (!roomIsGood)
-		{
-			//we failed!
-			Debug.Log("NO ROoms THAT FIT, THIS IS BAAAAD! ... but should never happen!");
-		}
-		else
-		{
-			GeneratorDoor otherDoor = newRoom.GetComponent<Room>().GetFirstOpenDoor();
-			door.sharedDoor = otherDoor;
-			otherDoor.sharedDoor = door;
-
-			door.open = false;
-			newRoom.GetComponent<Room>().GetFirstOpenDoor().open = false;
-
-			rooms.Add(newRoom.GetComponent<Room>());
-
-			AddGlobalVoxels(newRoom.GetComponent<Volume>().voxels);
-			if (!lastRoom.hasOpenDoors()) openSet.Remove(lastRoom);
-			Debug.Log(lastRoom.gameObject.name + lastRoom.gameObject.transform.position + "Has been destroyed");
-			if (newRoom.GetComponent<Room>().hasOpenDoors()) openSet.Add(newRoom.GetComponent<Room>());
-			roomsCount++;
-			hasFinalRoomSpawned = true;
-			//generationComplete = true;
-			Debug.Log("Success! Spawned" + newRoom.gameObject.name + "at" + newRoom.transform.position);
-
-			//Debug.Log("Openset: " + openSet.Count);
-		}
-		//newRoom.GetComponent<Room>().GetFirstOpenDoor().open = true;
-		//if (lastRoom.hasOpenDoors()) openSet.Remove(lastRoom.GetComponent<Room>());
-		//
-		//if (newRoom.GetComponent<Room>().hasOpenDoors()) openSet.Add(newRoom.GetComponent<Room>());
-		//if (newRoom.GetComponent<Room>().hasOpenDoors())
-		//{
-		//	openSet.Remove(lastRoom.GetComponent<Room>());
-		//	Debug.Log("DESTROYED" + lastRoom.gameObject.name + lastRoom.gameObject.transform.position);
-		//	newRoom = (GameObject)Instantiate(endRoom);
-		//	//openSet.Add(newRoom.GetComponent<Room>());
-		//	GeneratorDoor otherDoor = newRoom.GetComponent<Room>().GetFirstOpenDoor();
-
-
-		//	Debug.Log("Room to try has spawned" + gameObject.name +"at" + gameObject.transform.position);
-		//	newRoom.transform.parent = this.gameObject.transform;
-		//	door = ConnectRooms(lastRoom, newRoom.GetComponent<Room>());
-		//	door.sharedDoor = otherDoor;
-		//	otherDoor.sharedDoor = door;
-		//	door.open = true;
-
-		//}
-
-		//
-		#endregion
-	}
-	#endregion
 	#region Normalise Angle
 	private float NormalizeAngle(int rotation)
 	{
@@ -881,16 +569,41 @@ public class DungeonGenerator : MonoBehaviour
 			if (list[i].doors.Count == 1)
 			{
 				roomsWithOneDoor.Add(list[i]);
+				//foreach(var roomWithOneDoor in roomsWithOneDoor)
+				//{
+
+				//	Transform spawnPoint = GameObject.Find("KeySpawn").GetComponent<Transform>();
+				//	keySpawnPoints.Add(spawnPoint);
+				//	for (int s = 0; s < keySpawnPoints.Count; s++)
+				//	{
+				//		int chance = Random.Range(0, keySpawnPoints.Count - 1);
+				//		if(keySpawned == false)
+				//		{
+				//			Instantiate(key, keySpawnPoints[chance].position, keySpawnPoints[chance].rotation);
+				//			keySpawned = true;
+				//		}
+				//	}
+				//}
+				for (int s = 0; s < i; s++)
+				{
+					int chance = Random.Range(0, i - 1);
+					Debug.Log(i + "");
+				}
 				//Debug.Log("room : " + i);
 			}
 		}
+		//foreach (var roomWithOneDoor in roomsWithOneDoor)
+		//{
+
+		//	Transform spawnPoint = GameObject.Find("KeySpawn").GetComponent<Transform>();
+
+		//	int value = Random.Range(0, roomsWithOneDoor.Count);
+		//	Instantiate(key, spawnPoint.transform.position, spawnPoint.transform.rotation);
+		//}
 
 		hasAllTheEndRoomsSpawned = true;
 		amountEndRoomsSpawned++;
 		return roomsWithOneDoor;
-
-
-
 
 	}
 	#endregion
