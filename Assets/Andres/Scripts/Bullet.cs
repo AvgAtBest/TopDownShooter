5ad660cac649cd0d23e8d13f0c3c3e759e15313b
AveAmicus2250 <42423943+AveAmicus2250@users.noreply.github.com>
2019-06-14 17:08:38 +1000

    GUI

--- Assets/Andres/Scripts/Bullet.cs
+++ Assets/Andres/Scripts/Bullet.cs
@@ -1,20 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float bulletSpeed = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate ()
     {
-        
+        rb.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+        DestroyBullet();
     }
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "Enemy")
@@ -24,5 +31,9 @@ public class Bullet : MonoBehaviour
             gameObject.SetActive(false);
         }
     }
-
+    private void DestroyBullet()
+    {
+        Destroy(gameObject, 100);
+    }
+    
 }
---
+++ Assets/Andres/Scripts/Pause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+    void ActivateMenu()
+    {
+        pauseMenuUI.SetActive(true);
+    }
+    void DeactivateMenu()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+}
