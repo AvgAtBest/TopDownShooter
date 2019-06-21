8d1335fcfdce00c01abb6383d430ac3d47fbd57c
AveAmicus2250 <42423943+AveAmicus2250@users.noreply.github.com>
2019-06-21 19:15:23 +1000

    Hesitant Push

---
+++ Assets/FadeyBoi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeyBoi : MonoBehaviour
+{
+    public Image mySprite;
+    
+    public void SetSpriteTransparency(float transparency = .2f)
+    {
+        Color newColor = mySprite.color;
+        newColor.a = transparency;
+        mySprite.color = newColor;
+    }
+}
